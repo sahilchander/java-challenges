3895b45cc0cc7f05f69253969c428677e24277ae
Sahil <isahilchanderr@gmail.com>
2026-04-14 19:57:24 +0530

    Added condition-based programs (positive/negative, odd/even, greatest, leap year, grading, age category

---
+++ src/conditions/GradeChecker.java
@@ -0,0 +1,27 @@
+package conditions;
+
+import java.util.Scanner;
+
+public class GradeChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Grade Checker!!\n");
+
+        System.out.print("Please Enter Your Marks: ");
+        int marks = input.nextInt();
+
+        if (marks > 100 || marks < 0) {
+            System.out.println("\n\t Please Enter Valid Marks!!");
+        } else if (marks >= 90) {
+            System.out.println("\n\t Congratulation Your Got \"A\" ");
+        } else if (marks >= 75 ) {
+            System.out.println("\n\t Congratulation Your Got \"B\" ");
+        } else if (marks >= 60 ) {
+            System.out.println("\n\t  Your Got \"C\" ");
+        } else if (marks >= 30 ) {
+            System.out.println("\n\t  Your Got \"D\" ");
+        } else {
+            System.out.println("\n\t Your Failed");
+        }
+    }
+}
