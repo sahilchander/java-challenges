76d27ba607799841f67501d61636062706d22c99
Sahil <isahilchanderr@gmail.com>
2026-04-21 22:24:16 +0530

    Day 9 (Apr 21): Course practice started
    - Solved: 4 problems (easy)
    - Covered: basic problem solving (absolute difference, discount, divisibility, volume)
    - Added: 30 course questions (10 easy / 10 medium / 10 hard)

---
+++ src/FullPartOneTestQuestions/Easy/VolumeOfCylinder.java
@@ -0,0 +1,23 @@
+package FullPartOneTestQuestions.Easy;
+
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Volume of cylinder calculator");
+
+        System.out.print("Please enter radius of cylinder: ");
+        float radius = input.nextFloat();
+
+        System.out.print("Please enter your height of cylinder: ");
+        float height = input.nextFloat();
+
+        double volumeOFCylinder = volumeOFCylinder(radius,height);
+        System.out.println("Volume of your cylinder is " + volumeOFCylinder);
+
+    }
+    public static double volumeOFCylinder (float radius , float height) {
+        return (3.14 * radius * radius * height);
+    }
+}
